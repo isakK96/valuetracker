f1fa37eac0ac67db4d0edd38f0af3af951199683
isakK96 <isak.koski96@gmail.com>
2021-09-12 14:12:24 +0200

    add search history and starred tickers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import React, { useState } from "react";
-import Chart from "./Chart";
-import Search from "./Search";
+import React, { useState, useEffect } from "react";
+import Chart from "./components/Chart";
+import Search from "./components/Search";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [query, setQuery] = useState("IBM");
+  const [query, setQuery] = useState("");
   const [searchInput, setSearchInput] = useState("");
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [starredList, setStarredList] = useState([]);
+
   function handleSearch(e) {
     e.preventDefault();
-    setQuery(searchInput);
+    setQuery(searchInput.toUpperCase());
+  }
+
+  function handleStarClick() {
+    if (query && !starredList.includes(query)) {
+      setStarredList([query, ...starredList]);
+    }
+  }
+
+  useEffect(() => {
+    handleHistory();
+    console.log(searchHistory);
+  }, [query]);
+
+  // adds query to history if not already there / moves it to the start
+  function handleHistory() {
+    if (query) {
+      if (searchHistory.includes(query)) {
+        let newHistory = searchHistory.filter((item) => item !== query);
+        newHistory.unshift(query);
+        setSearchHistory(newHistory);
+      } else {
+        setSearchHistory([query, ...searchHistory]);
+      }
+    }
+  }
+  function handleHistoryClick(e) {
+    setQuery(e.target.text);
   }
   return (
     <div>
-      <Search
-        handleSearch={handleSearch}
-        searchInput={searchInput}
-        setSearchInput={setSearchInput}
-      />
-      <Chart query={query} />
+      <div className="top">
+        <Search
+          handleSearch={handleSearch}
+          searchInput={searchInput}
+          setSearchInput={setSearchInput}
+        />
+      </div>
+      <div>
+        <Sidebar
+          searchHistory={searchHistory}
+          starredList={starredList}
+          handleStarClick={handleStarClick}
+          handleHistoryClick={handleHistoryClick}
+        />
+        <Chart query={query} handleStarClick={handleStarClick} />
+      </div>
     </div>
   );
 }
