33ffc12c090d499b9a0ce5117209a0ca260bc130
isakK96 <isak.koski96@gmail.com>
2021-09-25 19:51:40 +0200

    add search suggestions

--- src/Api.js
+++ src/Api.js
@@ -5,15 +5,59 @@ const instance = axios.create({
 });
 
 export const getDailyValue = (symbol) => {
-  try {
-    return instance.get("", {
+  return instance
+    .get("", {
       params: {
         function: "TIME_SERIES_DAILY",
         symbol,
         apikey: "LXG3KFSHHFEET085",
       },
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
     });
-  } catch (error) {
-    return error;
-  }
+};
+
+export const getSearchSuggestion = (keywords) => {
+  return instance
+    .get("", {
+      params: {
+        function: "SYMBOL_SEARCH",
+        keywords,
+        apikey: "LXG3KFSHHFEET085",
+      },
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,82 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { getSearchSuggestion } from "../Api";
+import {
+  Button,
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material/";
+import debounce from "lodash.debounce";
 
-export default function Search({ searchInput, setSearchInput, handleSearch }) {
+export default function Search({
+  searchInput,
+  setSearchInput,
+  handleSearch,
+  handleSuggestionClick,
+}) {
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+
+  async function handleInput(e) {
+    if (e.target.value.length > 1) {
+      const result = await getSearchSuggestion(e.target.value);
+      formatSearchResults(result.data["bestMatches"]);
+    } else {
+      setSearchSuggestions([]);
+    }
+  }
+
+  function formatSearchResults(data) {
+    const list = Object.values(data)
+      .slice(0, 5)
+      .map((d) => (
+        <ListItem
+          key={d["1. symbol"]}
+          button
+          onClick={() => {
+            handleSuggestionClick(d);
+            setSearchSuggestions([]);
+          }}
+        >
+          <ListItemText
+            sx={{ color: "primary.main" }}
+            //classes={{ primary: "primary-text" }}
+            primary={d["1. symbol"]}
+            secondary={d["2. name"]}
+          />
+        </ListItem>
+      ));
+    setSearchSuggestions(list);
+  }
+
   return (
-    <div>
-      <form onSubmit={handleSearch} className="">
-        <label className="" htmlFor="searchInput">
-          {" "}
-        </label>
-        <input
-          type="text"
-          name="searchInput"
-          className=""
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button type="submit" className="">
+    <div className="form-container">
+      <form onSubmit={handleSearch} className="search-form">
+        <div className="search-wrapper">
+          <TextField
+            TextField
+            id="outlined-basic"
+            variant="outlined"
+            type="text"
+            name="searchInput"
+            className=""
+            autoComplete="off"
+            value={searchInput}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+              handleInput(e);
+            }}
+          />
+          <List
+            sx={{ position: "absolute", marginTop: "3.5rem" }}
+            className="suggestion-list"
+          >
+            {searchSuggestions}
+          </List>
+        </div>
+        <Button variant="contained" type="submit" className="">
           Search
-        </button>
+        </Button>
       </form>
     </div>
   );
